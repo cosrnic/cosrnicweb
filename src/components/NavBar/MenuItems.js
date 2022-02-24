5d564bc736227092401fc16de97610768f3879b6
Cosmic-0022 <75631523+cosmllc@users.noreply.github.com>
2022-02-24 18:31:42 +0000

    Components

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Projects',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Gallery',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About Me',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+]
---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { MenuItems } from './MenuItems';
+import logo from './logo.png'
+import './Navbar.css'
+
+
+class Navbar extends Component {
+    state = {
+        clicked: false
+    }
+
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render() {
+        return(
+            <nav className='NavbarItems'>
+                <h1 className='navbar-logo'>CosmicDEV <img src={logo} alt="logo" className='vsc-octo'></img></h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return(
+                            <li><a className={item.cName} href={item.url}>{item.title}</a></li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
